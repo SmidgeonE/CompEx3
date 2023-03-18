d01dbbfeb36a01ed69c2a6f136e72cb380567378
Smidgeon <finlaynugent@gmail.com>
2023-03-18 20:58:59 +0000

    Initial commit

---
+++ Ex3.py
@@ -0,0 +1,14 @@
+# $$
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import os
+
+G = 6.67E-11
+Mearth = 5.972E24
+Mmoon = 7.348E22
+Rearth = 6371000
+Msat = 1
+
+
+# $$

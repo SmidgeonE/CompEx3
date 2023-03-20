4b728ff344a8a0b484fe1eb959817812a64be0ec
Smidgeon <finlaynugent@gmail.com>
2023-03-20 01:34:18 +0000

    Added comments

--- Ex3.py
+++ Ex3.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import os
 
+# Useful constants for the entire script
+
 G = 6.67E-11
 Mearth = 5.972E24
 Mmoon = 7.348E22
@@ -14,6 +16,7 @@ moonY = 384400000
 moonX = 0
 
 
+# Functions to define the acceleration caused by the earth, or moon
 def accelX(x, y, shouldModelMoon=False):
     if shouldModelMoon:
         return (-G * Mearth * x) / ((x ** 2 + y ** 2) ** (3 / 2)) +\
@@ -28,22 +31,29 @@ def accelY(x, y, shouldModelMoon=False):
     else:
         return (-G * Mearth * y) / ((x ** 2 + y ** 2) ** (3 / 2))
 
+
+# Main function the evaluates the trajectory based on given initial conditions
 def EvalRK4(initVel=[0,0], initDisp=[0,0],
             numPoints=1000, simTime=9000,
             plotXY=True, plotE=True,
             modelMoon=False):
+    # Finding step size
     h = simTime / numPoints
     vx, vy, x, y = np.zeros(numPoints), np.zeros(numPoints), np.zeros(numPoints), np.zeros(numPoints)
     t = np.linspace(0, simTime, numPoints)
+    # Applying initial conditions
     vx[0], vy[0], x[0], y[0] = initVel[0], initVel[1], initDisp[0], initDisp[1]
 
+    # Iterates over all of the time steps
     for i, dt in enumerate(t):
+        # If its the last time step, dont integrate.
         if dt == t[-1]:
             continue
         # k arrays are 2d 4x4 arrays that go 1,2,3,4 then kx, ky, kvx, kvy
         k = np.zeros((4, 4))
         k[0] = np.array([vx[i], vy[i], accelX(x[i], y[i], shouldModelMoon=modelMoon), accelY(x[i], y[i], shouldModelMoon=modelMoon)])
 
+        # Determining the k constants
         for j in range(k.shape[0]-1):
             if j == range(k.shape[0]-1)[-1]:
                 k[j + 1] = np.array([vx[i] + h * k[j][2], vy[i] + h * k[j][3],
@@ -55,6 +65,7 @@ def EvalRK4(initVel=[0,0], initDisp=[0,0],
                                  accelY(x[i] + h * k[j][0] / 2, y[i] + h * k[j][1] / 2, shouldModelMoon=modelMoon)])
 
 
+        # This then evaluates the algorithm with the ks that were just determined
         multArray = np.array([1, 2, 2, 1])
         x[i+1] = x[i] + (h/6)*np.sum(k[:,0]*multArray)
         y[i+1] = y[i] + (h/6)*np.sum(k[:,1]*multArray)
@@ -89,6 +100,7 @@ def EvalRK4(initVel=[0,0], initDisp=[0,0],
     if plotXY:
         plt.figure(0)
 
+        # Plotting pictures of earth and moon if necessary
         earth = np.asarray(Image.open(os.getcwd()+"/earth.PNG"))
         plt.imshow(earth, extent=(-Rearth, Rearth, -Rearth, Rearth), alpha=0.9)
 
@@ -96,7 +108,7 @@ def EvalRK4(initVel=[0,0], initDisp=[0,0],
             moon = np.asarray(Image.open(os.getcwd()+"/moon.PNG"))
             plt.imshow(moon, extent=(-Rmoon, Rmoon, -Rmoon+moonY, Rmoon+moonY), alpha=0.9)
 
-        plt.plot(x, y, label='Trajectory', color='lightblue', lw=0.5)
+        plt.plot(x, y, label='Trajectory', color='lightblue', lw=2)
         plt.scatter(x[0], y[0], marker='x', color='green', label='Initial Point', s=12)
 
         if len(x) != numPoints:
@@ -104,8 +116,10 @@ def EvalRK4(initVel=[0,0], initDisp=[0,0],
             # If the length of the x array isnt the numPoints parameter, it must have crashed.
             plt.plot(x[-1], y[-1], marker='x', color='red', label='Crash Site')
 
+
+        # Tries to make the limits of the graph look less awful
         lim = np.max([np.abs(x), np.abs(y)])
-        border = 1
+        border = 0.8
         lim *= border
         order = int(np.log10(lim))
         centre = (np.mean(x), np.mean(y))
@@ -135,12 +149,11 @@ def EvalRK4(initVel=[0,0], initDisp=[0,0],
         plt.legend()
         plt.show()
 
+# Main UI loop
+
 print('------\nWelcome to Exercise 4\n\nObjectives:\n'
       '--Using computational tools and knowledge to solve a real world orbital problem.'
       '\n--Utilising Runge-Kutta algorithms to computationally solve orbital motion equations.')
-
-
-# Loops contains the UI that loops until the user quits.
 
 while True:
     answer = input('\nPlease enter one of the following letters, or q to quit:\n'
@@ -150,10 +163,11 @@ while True:
                    ' --(d) Satellite orbit around Earth (Comet Passing Slow)\n'
                    ' --(e) Satellite orbit around Earth (Circular) (With Moon Modeled)\n'
                    ' --(f) Satellite orbit around Moon and Earth\n'
-                   ' --(g) Satellite orbit around Moon and Earth (Crash on Moon)\n')
+                   ' --(g) Satellite orbit around Moon and Earth (Crash on Moon)\n'
+                   ' --(h) Custom orbit\n')
 
     if answer == 'a':
-        EvalRK4([0, 8300], [6700000, 0])
+        EvalRK4([0, 7750], [6700000, 0], simTime=9000)
 
     if answer == 'b':
         EvalRK4([0, 9500], [6700000, 0], simTime=18000)
@@ -168,10 +182,10 @@ while True:
         EvalRK4([0, 8300], [6700000, 0], modelMoon=True)
 
     if answer == 'f':
-        EvalRK4([10796, 0], [0, -6700000], modelMoon=True, simTime=1559200, numPoints=50000)
+        EvalRK4([10796, 0], [0, -6700000], modelMoon=True, simTime=829200, numPoints=50000)
 
     if answer == 'g':
-        EvalRK4([10793, 0], [0, -6700000], modelMoon=True, simTime=959200, numPoints=50000)
+        EvalRK4([10793, 0], [0, -6700000], modelMoon=True, simTime=1559200, numPoints=50000)
 
     elif answer != 'q':
         print("That was not a valid answer")
